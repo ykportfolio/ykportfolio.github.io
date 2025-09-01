f0d04c7acd2af5fac211091abf81c43e8d251cef
Yuliia Karas <yuliia.karas@example.com>
2025-09-01 15:57:08 +0300

    deleted unused script

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,32 +11,4 @@ document.addEventListener('DOMContentLoaded', function() {
     console.log('Burger or navLinks not found'); 
   }
 });
-
-document.querySelector("#myForm").addEventListener("submit", function (e) {
-  e.preventDefault(); 
-  const form = e.target;
-  console.log("Form submitted, redirect URL:", form.dataset.next); 
-
-  fetch(form.action, {
-    method: form.method,
-    body: new FormData(form),
-    headers: { "Accept": "application/json" },
-    redirect: "follow" 
-  })
-  .then(response => {
-    console.log("Response status:", response.status); 
-    if (response.redirected) {
-      console.log("Redirected to:", response.url);
-      window.location.href = form.dataset.next; 
-    } else if (response.ok) {
-      window.location.href = form.dataset.next; 
-    } else {
-      alert("There was a problem submitting the form.");
-    }
-  })
-  .catch(error => {
-    console.error("Error:", error);
-    alert("An error occurred while submitting the form.");
-  });
-});
   
